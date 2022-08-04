c9a1de9bb6bdcb27d56d51a79d214fe5ea22842b
mavel <pavel@mavel.cc>
2022-08-04 14:33:13 +0300

    clean up

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from random import randint
 import requests
 from dotenv import load_dotenv
 
-GROUP_ID = 214611066
 API_VERSION = 5.131
 
 
@@ -41,6 +40,9 @@ def upload_comic(comic_path: str) -> dict:
     return save_wall_photo(uploaded_photo_meta)
 
 
+# This function exists due to the peculiarity of VK API,
+# wherein you need to save an uploaded photo to an album
+# before you can post it in the group.
 def save_wall_photo(uploaded_photo_meta: dict) -> dict:
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
     data = {
@@ -48,7 +50,7 @@ def save_wall_photo(uploaded_photo_meta: dict) -> dict:
         'photo': uploaded_photo_meta['photo'],
         'server': uploaded_photo_meta['server'],
         'hash': uploaded_photo_meta['hash'],
-        'group_id': GROUP_ID,
+        'group_id': group_id,
         'v': API_VERSION,
     }
     response = requests.post(url, data=data)
@@ -58,7 +60,7 @@ def save_wall_photo(uploaded_photo_meta: dict) -> dict:
     if 'response' in response.json():
         return response.json()['response'][0]
     else:
-        raise requests.exceptions.RequestException
+        raise requests.exceptions.RequestException('The request to VK returned an error.')
 
 
 def post_comic(uploaded_photo: dict, alt: str) -> dict:
@@ -66,7 +68,7 @@ def post_comic(uploaded_photo: dict, alt: str) -> dict:
     data = {
         'access_token': access_token,
         'v': API_VERSION,
-        'owner_id': -GROUP_ID,
+        'owner_id': -group_id,
         'from_group': True,
         'message': alt,
         'attachments': f"photo{uploaded_photo['owner_id']}_{uploaded_photo['id']}",
@@ -78,7 +80,7 @@ def post_comic(uploaded_photo: dict, alt: str) -> dict:
     if 'response' in response.json():
         return response.json()
     else:
-        raise requests.exceptions.RequestException
+        raise requests.exceptions.RequestException('The request to VK returned an error.')
 
 
 def get_upload_server() -> dict:
@@ -86,7 +88,7 @@ def get_upload_server() -> dict:
     params = {
         'access_token': access_token,
         'v': API_VERSION,
-        'group_id': GROUP_ID,
+        'group_id': group_id,
     }
     response = requests.get(url, params=params)
 
@@ -94,7 +96,7 @@ def get_upload_server() -> dict:
     if 'response' in response.json():
         return response.json()
     else:
-        raise requests.exceptions.RequestException
+        raise requests.exceptions.RequestException('The request to VK returned an error.')
 
 
 def save_random_comic_and_alt() -> dict:
@@ -102,28 +104,25 @@ def save_random_comic_and_alt() -> dict:
     response = requests.get(latest_comic_url)
     response.raise_for_status()
     current_number = response.json()['num']
-    comic = None
-    while not comic:
-        random_number = randint(1, current_number)
-        comic = save_remote_comic_and_alt(random_number)
-    return comic
+    random_number = randint(1, current_number)
+    return save_remote_comic_and_alt(random_number)
 
 
-def save_and_post_random_comic() -> str:
+def save_and_post_random_comic() -> None:
     comic = save_random_comic_and_alt()
     try:
         uploaded_comic = upload_comic(comic['file_path'])
         post_response = post_comic(uploaded_comic, comic['alt'])
     finally:
         os.remove(comic['file_path'])
-    if 'response' in post_response:
-        return 'Random comic posted.'
-    else:
-        return 'Error, no comic was posted.'
+    if 'response' not in post_response:
+        raise requests.exceptions.RequestException('The request to VK returned an error.')
 
 
 if __name__ == "__main__":
     load_dotenv()
     access_token = os.getenv('ACCESS_TOKEN')
     upload_url = os.getenv('UPLOAD_URL')
-    print(save_and_post_random_comic())
+    group_id = int(os.getenv('GROUP_ID'))
+    save_and_post_random_comic()
+    print('A random comic has been posted.')
